1a7c403fa94db7c61381285f6294c9dc56ff1e26
Soma <somatekumalla@gmail.com>
2023-08-13 14:13:41 +0000

    Added SLF4J logging to CourseRetriever

--- java1/my-app/src/main/java/com/soma/app/cli/CourseRetriever.java
+++ java1/my-app/src/main/java/com/soma/app/cli/CourseRetriever.java
@@ -1,7 +1,30 @@
 package com.soma.app.cli;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class CourseRetriever {
+
+    private static final Logger LOG = LoggerFactory.getLogger(CourseRetriever.class);
     public static void main(String... args) {
-        System.out.println("CourseRetriever started");
+        LOG.info("CourseRetriever started");
+
+        if (args.length == 0) {
+            LOG.warn("Please provide an author name as first argument.");
+            return;
+        }
+
+        try {
+            retrieveCourses(args[0]);
+        } catch (Exception e) {
+            LOG.error("Unexpected error.", e);
+            // e.printStackTrace();
+        }
+        
     }
+
+    private static void retrieveCourses(String authorId) {
+        LOG.info("Retrieving courses for author '{}'", authorId);
+        }
+        
 }
