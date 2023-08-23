a86938edc7d83851064de3223c9cacff2505815f
Soma <somatekumalla@gmail.com>
2023-08-23 07:10:34 +0000

    Updated pom.xml and created parameterized tests with junit

--- java1/my-app/src/test/java/com/soma/app/cli/service/PluralsightCourseTest.java
+++ java1/my-app/src/test/java/com/soma/app/cli/service/PluralsightCourseTest.java
@@ -2,32 +2,23 @@ package com.soma.app.cli.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-// import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 
 class PluralsightCourseTest {
 
-    // Note - We want to replace these tests with parameterized tests.
-    @Test
-    void durationInMinutes() {        
-        PluralsightCourse course = 
-        new PluralsightCourse("id", "Test Course", "00:05:37", "url", false);
-        assertEquals(5, course.durationInMinutes());
-    }
-
-    @Test
-    void durationInMinutesOverHour() {        
-        PluralsightCourse course = 
-        new PluralsightCourse("id", "Test Course", "01:08:54.9613330", "url", false);
-        assertEquals(68, course.durationInMinutes());
-    }
-
-    @Test
-    void durationInMinutesZero() {        
-        PluralsightCourse course = 
-        new PluralsightCourse("id", "Test Course", "00:00:00", "url", false);
-        assertEquals(0, course.durationInMinutes());
-    }
+    @ParameterizedTest
+    @CsvSource(textBlock = """
+            01:08:54.9613330, 68
+            00:05:03, 5
+            00:00:00, 0
+            """)
+    void durationInMinutes(String input, long expected) {
+        PluralsightCourse course =
+        new PluralsightCourse("id", "Test Course", input, "url", false);
+        
+        assertEquals(expected, course.durationInMinutes());
+        }
+    
 }
